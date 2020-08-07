573f6d081d8b11158014eff8c5649719383f0f47
Franklin Ikeh <69267493+fr4nkl1n-1k3h@users.noreply.github.com>
2020-08-07 11:48:08 +0100

    Update Password_generator.py
    randomChar function names updated

--- Password_generator.py
+++ Password_generator.py
@@ -1,24 +1,29 @@
-#Password generator by Fr4nkl1n 1k3h
-#The randomChar module was designed by Fr4nkl1n 1k3h
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Password generator by Fr4nkl1n 1k3h
+# The randomChar module was designed by Fr4nkl1n 1k3h
+
 from random import shuffle
 import randomChar as rc
 
 randomLetter = rc.letter()
-randomNumber = rc.number()
+randomNumber = rc.digit()
 randomSymbol = rc.symbol()
-randomUpperCaseLetter = rc.letterUpper()
+randomUpperCaseLetter = rc.upcase()
+
 
 def generatePassword(length):
-	"""This function generates a password with random characters"""
-	charList = [randomLetter, randomNumber, randomSymbol, randomUpperCaseLetter]
-	
-	for i in range(length - 4):
-		charList.append(rc.all())
-		
-	shuffle(charList)
-	
-	return "".join(charList)
-	
+    """This function generates a password with random characters"""
+    charList = []
+
+    for i in range(length):
+        charList.append(rc.all())
+
+    shuffle(charList)
+    return ''.join(charList)
+
+
 print("""
 	#####################
 	## Welcome to my   ##
@@ -26,16 +31,17 @@ print("""
 	## generator       ##
 	#####################
 	""")
-passwordLength = int(input("Type in the length of your password: "))
+passwordLength = int(input('Type in the length of your password: '))
 password = generatePassword(passwordLength)
 print("""
 	######################################
 	## Your newly generated password is ##
 	######################################
-	\n\t\t{}\n
+
+\t\t{}
+
 	######################################
 	## Thanks for using my random       ##
 	## password generator               ##
 	######################################
 	""".format(password))
-		
